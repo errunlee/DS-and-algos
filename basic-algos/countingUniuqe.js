b79672589ae0ba49f7392aafffc86ce5727f7f7c
errunlee <32530613+errunlee@users.noreply.github.com>
2022-12-24 11:19:23 +0545

    some-changes

--- basic-algos/countingUniuqe.js
+++ basic-algos/countingUniuqe.js
@@ -1,18 +1,24 @@
 //Counting unique numbers;
+//              m        i
+// [1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 7, 7] => 7
 
-//[1,1,1,2,2,3,4,5,6,7,7,7,7] => 7
+//logic
+//i=0,j=1;
+//array[i]!==array[j]=>i++; array[i]=array[j]
 
-function count(arr){
-    let countIndex=0;
-    let m=0;
-  for(let i=0;i<arr.length;i++){
-    if(arr[m]!==arr[i]){
-        arr[m]=arr[i];
-        m++;
+function count(arr) {
+  let countIndex = 0;
+  let m = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[m] !== arr[i]) {
+      m++;
+      arr[m] = arr[i];
+      countIndex++;
     }
   }
+  return countIndex+1; //+1 because count started from 0;(m=0)
 }
 
-let input=[1,1,1,2,2,3,4,5,6,7,7,7,7];
-let result=count(input);
-// console.log(result)
+let input = [1, 1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 7, 7];
+let result = count(input);
+console.log(result)
